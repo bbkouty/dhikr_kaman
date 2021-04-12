f7e2828b31a5e31e6b918d66d62fb6b37ca85a1b
bbkouty <souare.boubacar.biro@gmail.com>
2021-04-12 13:45:43 +0000

    12/04

--- lib/Tasbih.dart
+++ lib/Tasbih.dart
@@ -17,9 +17,9 @@ class _Tasbih extends State<Tasbih> {
   bool numbered = true;
   bool buzzed = true;
   int increment = 0;
-  int total=0;
+  int total = 0;
 
-  void initialise(){
+  void initialise() {
     setState(() {
       increment = 1;
     });
@@ -38,7 +38,7 @@ class _Tasbih extends State<Tasbih> {
         ),
         actions: [
           IconButton(
-            icon: Icon( buzzed ? Icons.vibration : Icons.notifications_off),
+            icon: Icon(buzzed ? Icons.vibration : Icons.notifications_off),
             onPressed: () {
               setState(() {
                 buzzed = !buzzed;
@@ -47,22 +47,21 @@ class _Tasbih extends State<Tasbih> {
           ),
           IconButton(
             icon: TextButton(
-              onPressed: (){
+              onPressed: () {
                 setState(() {
-                numbered = !numbered;
+                  numbered = !numbered;
                 });
-                numbered && increment >33 ? initialise() : increment=increment ;
+                numbered && increment > 33
+                    ? initialise()
+                    : increment = increment;
               },
-              child: Text(numbered ? '33': '99',
+              child: Text(
+                numbered ? '33' : '99',
                 style: Theme.of(context).textTheme.caption,
               ),
               style: TextButton.styleFrom(
                 shape: CircleBorder(
-                  side: BorderSide(
-                    width: 2.0,
-                    color: Colors.white
-                  )
-                ),
+                    side: BorderSide(width: 2.0, color: Colors.white)),
               ),
             ),
           ),
@@ -70,25 +69,26 @@ class _Tasbih extends State<Tasbih> {
             icon: Icon(Icons.refresh),
             onPressed: () {
               setState(() {
-                increment=0;
-                total=0;
+                increment = 0;
+                total = 0;
               });
             },
           ),
         ],
       ),
-      bottomNavigationBar: Container(
-        height: 30,
-        decoration: BoxDecoration(
-          color: Colors.grey.shade800
-        ),
-        child: Center(
-          child: Text( 'الله سبحانه وتعالى',
-            style: TextStyle(
-              fontWeight: FontWeight.w800,
-              fontSize: 18.5,
-              height: 1.5,
-              color: Colors.white
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 30,
+          decoration: BoxDecoration(color: Colors.grey.shade800),
+          child: Center(
+            child: Text(
+              'الله سبحانه وتعالى',
+              style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  fontSize: 18.5,
+                  height: 1.5,
+                  color: Colors.white),
             ),
           ),
         ),
@@ -99,11 +99,8 @@ class _Tasbih extends State<Tasbih> {
             aspectRatio: 7 / 3,
             child: Container(
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/coran.jpg'),
-                  fit: BoxFit.fill
-                )
-              ),
+                  image: DecorationImage(
+                      image: AssetImage('assets/coran.jpg'), fit: BoxFit.fill)),
               //elevation: 7.0,
               //shadowColor: Colors.grey,
               child: Column(
@@ -115,79 +112,79 @@ class _Tasbih extends State<Tasbih> {
                       height: 100,
                       width: 100,
                       child: LiquidCircularProgressIndicator(
-                        value: numbered ? increment/33 : increment/99,
+                        value: numbered ? increment / 33 : increment / 99,
                         center: Text(
-                          numbered ? '$increment/33': '$increment/99',
+                          numbered ? '$increment/33' : '$increment/99',
                           style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.black
-                          ),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.black),
                         ),
                       ),
                     ),
                   ),
                   RichText(
-                    text: TextSpan(
-                      text: 'Total:   $total',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                        height: 1.5,
-                      ),
-                    )
-                  ),
+                      text: TextSpan(
+                    text: 'Total:   $total',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      height: 1.5,
+                    ),
+                  )),
                 ],
               ),
             ),
           ),
           Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade800
-                ),
-                child: ClipPath(
-                  clipper: RoundedDiagonalPathClipper(),
-                  child: Container(
-                    height: 400,
-                    decoration: BoxDecoration(
+            child: Container(
+              decoration: BoxDecoration(color: Colors.grey.shade800),
+              child: ClipPath(
+                clipper: RoundedDiagonalPathClipper(),
+                child: Container(
+                  height: 400,
+                  decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                      color: Colors.green
-                    ),
-                    child: InkWell(
-                      splashColor: Colors.white,
-                      hoverColor: Colors.white,
-                      onTap: () {
-                        if (buzzed) {Vibration.vibrate(duration: 250);}
-                        total ++;
-                        increment==33 && numbered ? initialise():
-                        !numbered && increment == 99 ? initialise() : setState(() {
-                          increment ++;
-                        });
-                        if(numbered && increment%33 == 0){Vibration.vibrate(duration: 500);}
-                        if(!numbered && increment%99 == 0){Vibration.vibrate(duration: 500);}
-                      },
-                      child: Center(
-                        child: Container(
-                          height: 300,
-                          decoration: BoxDecoration(
+                      color: Colors.green),
+                  child: InkWell(
+                    splashColor: Colors.white,
+                    hoverColor: Colors.white,
+                    onTap: () {
+                      if (buzzed) {
+                        Vibration.vibrate(duration: 250);
+                      }
+                      total++;
+                      increment == 33 && numbered
+                          ? initialise()
+                          : !numbered && increment == 99
+                              ? initialise()
+                              : setState(() {
+                                  increment++;
+                                });
+                      if (numbered && increment % 33 == 0) {
+                        Vibration.vibrate(duration: 500);
+                      }
+                      if (!numbered && increment % 99 == 0) {
+                        Vibration.vibrate(duration: 500);
+                      }
+                    },
+                    child: Center(
+                      child: Container(
+                        height: 300,
+                        decoration: BoxDecoration(
                             shape: BoxShape.circle,
-                            color: Colors.grey.shade800
-                          ),
-                          child: Center(
-                            child: Container(
-                              height: 200,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.green
-                              ),
-                              child: Center(
-                                child: Container(
-                                  height: 100,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: Colors.grey.shade800,
-                                  ),
+                            color: Colors.grey.shade800),
+                        child: Center(
+                          child: Container(
+                            height: 200,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.green),
+                            child: Center(
+                              child: Container(
+                                height: 100,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.grey.shade800,
                                 ),
                               ),
                             ),
@@ -199,8 +196,7 @@ class _Tasbih extends State<Tasbih> {
                 ),
               ),
             ),
-          //Row(),
-          //InkWell(),
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: Tasbih(title: 'Tasbih',)//MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Tasbih(title: 'Tasbih',)
     );
   }
 }
